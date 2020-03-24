324df5f4f2b7026e5ffe9bc81b89f4ec9a5098fd
Andrew Gillingham <andrew@leadsnearby.com>
2020-03-24 10:19:18 -0400

    Bump version number

--- lnb-tech-pro.php
+++ lnb-tech-pro.php
@@ -3,7 +3,7 @@
 Plugin Name: LeadsNearby Tech Profiles
 Plugin URI: http://leadsnearby.com
 Description: Creates Tech Profiles with Nearby Now Plugin capability.
-Version: 1.2.0
+Version: 1.5.0
 Author: Leads Nearby
 Author URI: http://leadsnearby.com
 License: GPLv2
